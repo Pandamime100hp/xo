8e804695f93aff20cbcf15a379a867094f14d642
Makarintosh <66727639+Pandamime100hp@users.noreply.github.com>
2024-05-02 11:00:21 +0200

    feat(player): added player class
    Added `.gitignore` file
    Added `*.pyc` file exclusion to `.gitignore`.
    Added `src/player.py` file.
    Added `Player` class to `src/player.py` file.
    Updated `src/person.py` file with abstract functions and initialisation.
    Added `increment_score` function to `src/person.py` to add 1 to the persons score.

--- src/person.py
+++ src/person.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from . import XOSymbol
 
@@ -7,4 +7,26 @@ class Person(ABC):
     symbol: XOSymbol
     nickname: str
     score: int
+
+    def __init__(self, symbol: XOSymbol, nickname: str) -> None:
+        self.symbol = symbol
+        self.nickname = nickname
+        self.score = 0
     
+    @property
+    @abstractmethod
+    def symbol(self) -> XOSymbol:
+        pass
+
+    @property
+    @abstractmethod
+    def nickname(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def score(self) -> int:
+        pass
+
+    def increment_score(self) -> None:
+        self.score += 1
