3eb3312cbe185a9248482c46da109ae92fe77c6a
Makar Emeliyanov <66727639+Pandamime100hp@users.noreply.github.com>
2024-05-08 09:43:23 +0200

    27 create test function for person class (#34)
    * feat(person): added tests
    
    Added unit tests for `Person` class.
    Added `.person` import to root `__init__.py`.
    Removed abstract properties as they were causing issues.
    Updated `.coveragerc` to remove all test folders from the report.
    
    * chore(documenting): added documentation to code
    
    * chore(documentation): added documentation
    
    Added documentation to `unit_tests/test_person.py`.

--- tests/unit_tests/test_person.py
+++ tests/unit_tests/test_person.py
@@ -8,20 +8,30 @@ class TestPerson(unittest.TestCase):
     person: Person
 
     def setUp(self):
+        """_summary_
+        """
         pass
 
     def tearDown(self):
+        """_summary_
+        """
         pass
 
     def test_person_x(self):
+        """_summary_
+        """
         self.person = Person(symbol=XOSymbol.X, nickname="")
         self.assertEqual(XOSymbol.X, self.person.symbol)
 
     def test_person_o(self):
+        """_summary_
+        """
         self.person = Person(symbol=XOSymbol.O, nickname="")
         self.assertEqual(XOSymbol.O, self.person.symbol)
 
     def test_increment_person_score(self):
+        """_summary_
+        """
         self.person = Person(symbol=XOSymbol.O, nickname="")
         self.person.increment_score()
         self.assertEqual(1, self.person.score)
