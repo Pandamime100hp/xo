ec2b7072592358b690ba391f817a8a9da93af87f
Makarintosh <66727639+Pandamime100hp@users.noreply.github.com>
2024-05-20 14:47:23 +0200

    feat(controller): added function to get user input
    Added a function `get_input` to get the users input.

--- src/controller.py
+++ src/controller.py
@@ -3,3 +3,10 @@ class Controller:
         """_summary_
         """
         pass
+
+    def get_input(self):
+        """Get user input during a decision."""
+        user_input: str = input("?>: ")
+        return user_input
+
+    # def self._process_user_input(self, user_input: str):
