fcad8f730cc9095adf1a174c23155bafa5bffbbc
Makarintosh <66727639+Pandamime100hp@users.noreply.github.com>
2024-06-01 12:02:45 +0100

    feat(game_state): added attributes for player and enemy
    Added `Player` and `Enemy` attributes to track persons infromation.
    Added `score_player` and `score_enemy` to keep track of the games score for player and enemy.

--- src/game/game_state.py
+++ src/game/game_state.py
@@ -1,8 +1,14 @@
-from src.enums import State
+from src.actors.enemy import Enemy
+from src.actors.player import Player
+from src.enums import State, XOSymbol
 
 
 class GameState:
     state: State
+    player: Player
+    enemy: Enemy
+    score_player: int = 0
+    score_enemy: int = 0
 
     def __init__(self) -> None:
         """
